339ae2878565e6c5f5ab9dfaf71a8e30cf13ddc8
Rafa <rafael.rodriguez@uca.es>
2015-10-07 11:12:07 +0200

    Added first example for mesh1d

---
+++ examples/mesh1d_example.f90
@@ -0,0 +1,23 @@
+program mesh1d_example
+
+  use nrtypes
+  use mesh, only : mesh1d, init, ncells, nvertices
+  implicit none
+
+  integer(long) :: i, nc, nv
+  type(mesh1d) :: Th
+  call init(Th, x1=0.0_dp, x2=1.0_dp, ncells=10)
+
+  nc = ncells(Th)
+  print *, "Number of cells in Th:", nc
+  do i=1, nc
+     print *, "Cell", i, ": vertices...", Th%vertices(i,:)
+  end do
+
+  nv = nvertices(Th)
+  print *, "Number of vertices in Th:", nv
+  do i=1, nv
+     print *, "Vertex", i, ": coordinates...", Th%coordinates(i,:)
+  end do
+
+end program mesh1d_example
